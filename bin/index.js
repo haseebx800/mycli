da7ed32ecb341ca5e775081957ee7c2fe676b25b
Abdul Haseeb <abdulhaseeb9379@gmail.com>
2026-09-16 10:16:25 +0530

    feat: support multi-word names

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,32 @@
+#!/usr/bin/env node
+
+const command = process.argv[2];
+
 if (command === "hello") {
-    // ...
+    console.log("Hello from Haseeb CLI 🚀");
 }
 
 else if (command === "help") {
-    // ...
+    console.log(`
+Haseeb CLI
+
+Usage:
+  haseeb <command>
+
+Commands:
+  hello       Say hello
+  help        Show this help message
+  greet       Greet someone
+  --version   Show version
+`);
 }
 
 else if (command === "--version") {
-    // ...
+    console.log("1.0.0");
 }
 
 else if (command === "greet") {
-    const name = process.argv[3];
+    const name = process.argv.slice(3).join(" ");
 
     if (!name) {
         console.log("Please provide a name.");
